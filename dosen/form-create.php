b4498d31ff802600ec8e5e912f2d69d4191f3905
Rashad <46430370+rashadandredi@users.noreply.github.com>
2019-03-13 10:15:38 +0700

    selesai dikerjakan

---
+++ dosen/form-create.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <h3>Tambah Data Dosen</h3>
+
+    <form action="create.php" method="post">
+      <label>Nama dosen</label>
+      <input type="text" name="nama_dosen"><br>
+      <label>No. Telepon</label>
+      <input type="text" name="telp"><br>
+
+      <input type="submit" name="btnSimpan" value="Simpan">
+    </form>
+  </body>
+</html>
